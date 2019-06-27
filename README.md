25b58a03f5b980f9a3e8c31a34a7307996ccb9ed
Salomon BRYS <salomon@kodein.net>
2019-06-27 23:48:53 +0200

    Skeleton

---
+++ README.md
@@ -0,0 +1,6 @@
+Kotlin iOS Universal Framework
+==============================
+
+A Gradle plugin that generates a universal framework from Kotlin/Native frameworks and a binary associated podspec.
+
+Work In Progress ;)
---
+++ src/main/kotlin/com/github/salomonbrys/gradle/kotlin/nat/IosUniversalFrameworkPlugin.kt
@@ -0,0 +1,14 @@
+package com.github.salomonbrys.gradle.kotlin.nat
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class IosUniversalFrameworkPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) = project.applyPlugin()
+
+    private fun Project.applyPlugin() {
+
+    }
+
+}
